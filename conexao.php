23f237887ec358ec101e7c41072f16583663a3fc
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-30 16:08:58 -0300

    Criando a conexao com banco de dados.

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+  function conectar()
+  {
+    $host = "localhost";
+    $user = "root";
+    $passoword = "mudar@123";
+    $db = "sistema_novaera";
+
+    $con = mysqli_connect($host, $user, $passoword, $db);
+    if (!$con)
+      die("FALHA NA CONEXÃO <br>" . mysqli_connect_error());
+    return $con;
+  }
+
+  $conexao = conectar();
+
+?>
