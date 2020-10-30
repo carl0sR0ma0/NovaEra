c6e8dc8f578930330b618855be8726e3e137e978
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-30 18:48:08 -0300

    Importando JS para o projeto.

--- admin/header.php
+++ admin/header.php
@@ -3,6 +3,25 @@
 <head>
   <meta charset="UTF-8">
   <link rel="stylesheet" type="text/css" href="css/header.css">
+  <script type="text/javascript" src="../js/jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="../js/lightbox.js"></script>
+  <link rel="stylesheet" href="../css/lightbox.css">
+
+  <link rel="stylesheet" typ="text/css" href="../jquery.superbox.css" media="all">
+  <!-- <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script> -->
+  <script type="text/javascript" src="../jquery.superbox-min.js"></script>
+  <script type="text/javascript">
+    $(function () {
+      $superbox.settings = {
+        closeTxt: "Fechar",
+        loadTxt: "Carregando...",
+        nextTxt: "Next",
+        prevTxt: "Previous"
+      };
+
+      $.superbox();
+    });
+  </script>
   <?php include "../conexao.php"; ?>
 </head>
 <body>
