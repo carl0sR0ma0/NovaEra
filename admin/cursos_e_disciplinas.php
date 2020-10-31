69d65608dc2d26df4962366bfb6d7edc55fcc02d
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-30 23:31:30 -0300

    Criando o arquivo onde vai conter o CRUD de Cursos e Disciplinas; E modificando a logo no header.

---
+++ admin/cursos_e_disciplinas.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Cursos & Disciplinas</title>
+  <link rel="stylesheet" href="css/cursos_e_disciplinas.css">
+</head>
+<body>
+  <?php require "header.php"; ?>
+  <div id="caixa_preta"></div>
+  <!-- CADASTRAR CURSO -->
+
+  <?php require "footer.php"; ?>
+</body>
+</html>
