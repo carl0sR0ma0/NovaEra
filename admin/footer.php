b9de71b805390d4d15403664f0c90a5db6be35fd
carl0sR0ma0 <cvpromao@gmail.com>
2020-10-30 22:53:44 -0300

    Implementando o footer.

---
+++ admin/footer.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">  
+  <link rel="stylesheet" href="css/footer.css">
+</head>
+<body>
+  <div id="box_rodape">
+    <h1 class="h1">
+      Escola Nova Era - © Desenvolvido pelo grupo de Programação Web - FATEC Taquaritinga
+    </h1>
+  </div>
+</body>
+</html>
--- admin/index.php
+++ admin/index.php
@@ -7,5 +7,7 @@
 </head>
 <body>
   <?php include "header.php"; ?>
+  
+  <?php include "footer.php"; ?>
 </body>
 </html>
